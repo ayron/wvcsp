219ab3e69efb0658bf1fe4b1f9a9c7c4db9de8b7
Ayron Catteau <ayronc@gmail.com>
2010-05-16 22:30:56 -0400

    Fixed enough so that it will at least now compile.

--- main.cc
+++ main.cc
@@ -1,20 +1,20 @@
 /*
- *  GrabVideo.c
+ *  main.c
  *
- *  Created by Dev Priya on 11/09/09.
- *  Copyright 2010 Robosense. All rights reserved.
+ *  TODO Discription
+ *
  *
  */
- 
- 
-#include "cvextra.h"
+
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
 #include <iostream>
 
 using namespace std;
 
 int main (int argc, char** argv)
 {
-	double threshold = atof(argv[1]);
+	//double threshold = atof(argv[1]);
 
 	// OpenCV data structure for handle to the cam
 	CvCapture* vidObj;
@@ -28,9 +28,6 @@ int main (int argc, char** argv)
  
 	// Creating structures to hold the captured and processed frames
 	IplImage *grabbedImg; //, *grayImg;
-	
-	// Class to handle RGB pixels
-	RgbImage imgA;
 
 	// Retrieve the captured frame
 	grabbedImg = cvRetrieveFrame(vidObj);
@@ -45,37 +42,12 @@ int main (int argc, char** argv)
 	//cvNamedWindow("ModifiedImage", CV_WINDOW_AUTOSIZE);
 	//cvMoveWindow("ModifiedImage", 640, 100);
 
-	// Will hold the corners of the square
-	CvPoint a, b;
-	// The colour we are looking for
-	Colour theColour(130,35,24);
-	
-	cout<<"Scanning for hue: "<<hue(theColour.r, theColour.g, theColour.b)<<" Threshold: "<<threshold<<endl;
 
   	// Loop until Esc key is pressed
-	while (1) {
- 	//for (int i = 0; i<10; i++) {
+	while (1) 
+    {
 		// This function is used to grab the frame from camera + retrieve the image from it
 		grabbedImg = cvQueryFrame(vidObj);
- 		
-		// Convert to RGB wrapper
-		imgA = grabbedImg;
-
-		//find_colour(a,b,imgA,theColour, threshold);
-
-		//cvRectangle(grabbedImg, a, b, cvScalar(255,255,255), 1);
-		//cvRectangle(grabbedImg, cvPoint(100,100), cvPoint(200,200), cvScalar(255,255,255), 1);
-		
-		for (int r = 0; r < imgA.h(); r++)
-		{
-			for (int c = 0;c<imgA.w(); c++)
-			{
-				if (fabs(hue((int)imgA[r][c].r,(int)imgA[r][c].g,(int)imgA[r][c].b) - hue(theColour.r, theColour.g, theColour.b)) < threshold )
-				{
-					cvLine(grabbedImg, cvPoint(c,r), cvPoint(c,r), cvScalar(255,255,255), 1);
-				}
-			}
-		}
 
 		// Display the captured frame in first Window
 		cvShowImage("CapturedImage", grabbedImg);
